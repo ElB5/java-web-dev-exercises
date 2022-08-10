3f8ce83f6ed87d31695a67994f58a4addef3a333
ElB5 <eb.brady.ellen@gmail.com>
2022-08-09 21:39:11 -0500

    exercise update

---
+++ src/exercises/Array.java
@@ -0,0 +1,34 @@
+package exercises;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+
+//        int[] intArray = {1, 1, 2, 3, 5, 8};
+//        Loop through the array and print out each value,
+//        for (int i : intArray) {
+//            System.out.println(i);
+//        }
+//        then modify the loop to only print the odd numbers.
+//        for (int i = 0; i <= intArray.length; i++) {
+//            if (intArray[i] % 2 != 0) {
+//                System.out.println(intArray[i]);
+//            }
+//        }
+//split string into array at each space:
+//        String str = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. " +
+//                "I will not eat them with a mouse.";
+//        String[] strArr = str.split(" ");
+//
+//        System.out.println(Arrays.toString(strArr));
+
+        //split string into array at each sentence:
+        String str = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. " +
+                "I will not eat them with a mouse.";
+        String[] strArr = str.split("\\.");
+
+        System.out.println(Arrays.toString(strArr));
+
+    }
+};
---
+++ src/exercises/HashMap.java
@@ -0,0 +1,39 @@
+package exercises;
+
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class HashMap {
+    public static void main(String[] args) {
+
+        java.util.HashMap<Integer, String> classRoster = new java.util.HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+        // Get student ID and name
+        do {
+            System.out.print("Student: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("ID: ");
+                Integer newID = input.nextInt();
+                classRoster.put(newID, newStudent);
+
+                input.nextLine();
+            }
+        } while (!newStudent.equals(""));
+
+        input.close();
+
+        System.out.println("\nClass roster:");
+
+        for (Map.Entry<Integer, String> student : classRoster.entrySet()) {
+            System.out.println(student.getValue() + "'s ID: " + student.getKey());
+        }
+
+        System.out.println("Number of students in roster: " + classRoster.size());
+    }
+}
