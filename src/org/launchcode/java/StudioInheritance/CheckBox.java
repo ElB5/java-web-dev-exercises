84962c0fa792455c168db0d8cd30fa64a9b67bef
ElB5 <eb.brady.ellen@gmail.com>
2022-09-21 22:55:03 -0500

    completed studio for inheritance

--- src/org/launchcode/java/StudioInheritance/CheckBox.java
+++
@@ -1,9 +0,0 @@
-package org.launchcode.java.StudioInheritance;
-
-public class CheckBox extends Question {
-
-    private String checkBoxQuestion = "Check all Items that need sunlight to survive.";
-
-
-
-}
